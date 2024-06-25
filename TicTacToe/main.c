951f663311855b9e980fca3160d884d845435914
sleinkaraman <162487122+sleinkaraman@users.noreply.github.com>
2024-06-25 16:25:35 +0300

    Initial Commit

---
+++ TicTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Selin Karaman on 25.06.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
